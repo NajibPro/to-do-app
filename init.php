cd1baf76dfbe185b854eeb933fd20adec59c6468
webdev <ishak.abassi@ensia.edu.dz>
2023-03-15 14:19:00 +0100

    le croissant est belle

--- init.php
+++ init.php
@@ -4,7 +4,7 @@ error_reporting(E_ERROR | E_PARSE);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 
-define('APP_LANG','en');
+define('APP_LANG', 'fr');
 
 include("lib/db.php");
 include("lib/utils.php");
@@ -12,9 +12,9 @@ include("lang/".APP_LANG.".php");
 
 //It is very stupid to share passwords within GIT, but for demostration, we will close our eyes on this principle.
 $dbhost = 'localhost';
-$dbuser = 'u229483663_todo';
-$dbpass = 'Ensia123456';
-$dbname = 'u229483663_todo';
+$dbuser = 'g1.1';
+$dbpass = 'g1.1g1.1';
+$dbname = 'todo_g1.1';
 
 $db = new db($dbhost, $dbuser, $dbpass, $dbname);
 
